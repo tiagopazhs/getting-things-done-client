c281b81df58717abb256b5380108c8bb100d8728
tiagopazhs <tiagopaz.gd@gmail.com>
2024-05-24 08:38:02 -0300

    feat: Create Actionslist component

---
+++ src/ActionsList.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
+function ActionList() {
+    const [actions, setActions] = useState<>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8022/actions')
+            .then((response) => {
+                setActions(response.data)
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Actions List</h2>
+            <ul>
+                {actions.map((action) => (
+                    <li key={action.id}>
+                        <strong>{action.name}</strong>
+                        <p>{action.description}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+
+}
+
+export default ActionList;
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import ActionsList from './ActionsList';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>This is the Getting things done project 🚀</h1>
-      <div className="card">
-        <p>
-          Here we will create the projects screen
-        </p>
-      </div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        This is an example of counter {count}
-      </button>
+      <h1>Getting things done 🚀</h1>
+      <ActionsList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
